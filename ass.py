61df46918d123402c194e28fca417e3f2c963e9a
snehalatha09 <67182676+snehalatha09@users.noreply.github.com>
2020-06-23 15:18:30 +0530

    Add files via upload

---
+++ ass.py
@@ -0,0 +1,27 @@
+#area of a circle
+import math
+r=int(input("enter the value of radius"))
+a=math.pi
+area= a*r*r
+print("area of the circle",area)
+# To generate random numbers between 1 to 100
+import random
+print(random.randrange(1,1000))
+#using math module
+from math import sin,cos,tan
+print(math.sin(60))
+print(math.cos(pi))
+print(math.tan(90))
+#area of regular polygon
+from math import tan, pi
+n_sides = int(input("input number of sides"))
+s_length=float(input("input the legth of the side"))
+p_area=n_sides*(s_length**2)/(4*tan(pi/n_sides))
+print("the are of the polygon is:",p_area)
+#using math module
+import math
+print(math.sqrt(400))
+print(math.ceil(23.56))
+print(math.floor(23.56))
+a=5^8
+print(a)
